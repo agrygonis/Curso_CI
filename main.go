d673941122776f0e600ca36f545982180262aaf0
agrygonis <agrygonis@gmail.com>
2023-09-03 22:59:48 -0300

    Primeiro Commit

---
+++ main.go
@@ -0,0 +1,13 @@
+//comentádo por grygonis
+
+package main
+
+import (
+	"github.com/guilhermeonrails/api-go-gin/database"
+	"github.com/guilhermeonrails/api-go-gin/routes"
+)
+
+func main() {
+	database.ConectaComBancoDeDados()
+	routes.HandleRequest()
+}
